6606b1f4bdf312d2c74a499ba650285376be0548
unknown <malondrasarian@gmail.com>
2025-05-02 19:59:20 +0800

    init: created base React app structure, installed axios and react-router-dom

---
+++ src/config/config.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const config = axios.create({
+    // baseURL: "",
+    baseURL: "http://localhost:5050",
+    // baseURL: "http://192.168.20.110:5050",
+});
+
+config.interceptors.request.use((cnfg) => {
+    const token = localStorage.getItem("token");
+    if(token){
+        cnfg.headers.Authorization = `Bearer ${token}`;
+    }
+    return cnfg;
+})
+
+export default config;
---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,62 @@
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+// import Login from '../pages/Login'
+// import NotFoundPage from "../pages/NotFoundPage";
+// import Header from "../components/Header";
+
+// // Issued Items
+// import Home from '../pages/Home'
+// import IssuedItemsAdd from '../pages/issued-items/Add'
+// import IssuedItemsEdit from '../pages/issued-items/Edit'
+
+
+// // Settings
+// import Settings from '../pages/settings/Index'
+
+// import RequireAuth from "./RequireAuth";
+// import AdminAuth from "./AdminAuth";
+// import ChangePassword from "../pages/ChangePassword";
+// import RequireAuthOnly from "./RequireAuthOnly";
+
+
+function AppRouter() {
+    return (
+        <Router>
+            <HeaderWrapper />
+
+            <Routes>
+                <Route path="/login" element={<Login />} />
+
+                <Route element={<RequireAuth />}>
+                    {/* HOME AND ISSUED ITEMS */}
+                    <Route path="/" element={<Home />} />
+                    {/* <Route path="/issued-items/add" element={<IssuedItemsAdd />} />
+                    <Route path="/issued-items/edit" element={<IssuedItemsEdit />} /> */}
+
+                    <Route element={<AdminAuth />}>
+                        {/* SETTING */}
+                        {/* <Route path='/settings' element={<Settings />} /> */}
+                    </Route>
+                </Route>
+                
+                {/* <Route element={<RequireAuthOnly />}>
+                    <Route path="/change-password" element={<ChangePassword />} />
+                </Route> */}
+
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </Router>
+    );
+}
+
+const HeaderWrapper = () => {
+    const token = localStorage.getItem("token");
+    
+    const path = useLocation();
+    const hideHeaderRoutes = ['/'];
+
+    if(hideHeaderRoutes.includes(path.pathname) && token){
+        // return <Header />
+    }
+}
+
+export default AppRouter;
