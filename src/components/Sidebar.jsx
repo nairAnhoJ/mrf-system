7cc7899065c1e33315246318bde29baf92a6e5ae
unknown <malondrasarian@gmail.com>
2025-05-20 11:14:48 +0800

    Dark Mode Toggle - Added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,44 +1,115 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import IconRenderer from './icons';
+import { NavLink } from 'react-router-dom';
+import { Me } from '../services/authService';
 
 const Sidebar = () => {
-    const [isFocus, setIsFocus] = useState(false);
+    const [me, setMe] = useState({});
+
+    const navItems = [
+        { key: "nonChargeable",label: "Non-Chargeable", path: "/non-chargeable" },
+        { key: "chargeable",label: "Chargeable", path: "/chargeable" },
+        { key: "settings",label: "Settings", path: "/settings" },
+    ];
+    
+    const [theme, setTheme] = useState(() => {
+        if(localStorage.getItem('dark') == null){
+            return 'light';
+        }else{
+            return localStorage.getItem('dark');
+        }
+    });
+
+    useEffect(() => {
+        if(theme == 'dark'){
+            document.body.classList.add("dark");
+        }else{
+            document.body.classList.remove("dark");
+        }
+    }, [theme]);
+
+    useEffect(() => {
+        const getMe = async() => {
+            try {
+                const response = await Me();
+                setMe(response.user);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getMe();
+    }, [])
+
+    const toggleTheme = () => {
+        if(theme == 'dark'){
+            setTheme('light');
+            localStorage.setItem('dark', 'light');
+        }else{
+            setTheme('dark');
+            localStorage.setItem('dark', 'dark');
+        }
+    };
+
+
 
     const handleLogout = () => {
         localStorage.removeItem("token");
         window.location.href = "/login";
     }
 
+
+
     return (
         <div className='h-[calc(100%-32px)] flex flex-col justify-between fixed w-24 hover:w-72 bg-neutral-800 text-neutral-300 overflow-hidden transition-all duration-300'>
             <div className='bg-white w-8 h-full absolute right-0 translate-x-1/2 rounded-l-2xl'></div>
-            <div className='w-[calc(100%-16px)] h-10 flex justify-center'><img src="/logo.ico" alt="logo" className='w-10 h-10'/></div>
+            <div>
+                <div className='w-[calc(100%-16px)] h-10 flex justify-center'><img src="/logo.ico" alt="logo" className='w-10 h-10'/></div>
 
-            <div className=''>
-                <button className='w-72 h-12 pl-6 border-s-4 border-neutral-800 hover:border-white hover:text-white flex whitespace-nowrap items-center cursor-pointer'>
-                    <IconRenderer name="nonChargeable" className='w-7 h-7' />
-                    <span className='ms-7 font-semibold'>Non-Chargeable</span>
-                </button>
-                <button className='w-72 h-12 pl-6 border-s-4 border-neutral-800 hover:border-white hover:text-white flex whitespace-nowrap items-center cursor-pointer'>
-                    <IconRenderer name="chargeable" className='w-7 h-7' />
-                    <span className='ms-7 font-semibold'>Chargeable</span>
-                </button>
-                <button className='w-72 h-12 pl-6 border-s-4 border-neutral-800 hover:border-white hover:text-white flex whitespace-nowrap items-center cursor-pointer'>
-                    <IconRenderer name="settings" className='w-7 h-7' />
-                    <span className='ms-7 font-semibold'>Settings</span>
-                </button>
-                <button onClick={handleLogout} className='w-72 h-12 pl-6 border-s-4 border-neutral-800 hover:border-white hover:text-white flex whitespace-nowrap items-center cursor-pointer'>
-                    <IconRenderer name="logout" className='w-7 h-7' />
-                    <span className='ms-7 font-semibold'>Log out</span>
-                </button>
+                <div className='mt-20'>
+                    {navItems.map((item) => (
+                        <NavLink to={item.path} key={item.path} className={({ isActive }) => `w-72 h-12 pl-6 border-s-4 hover:border-white hover:text-white flex whitespace-nowrap items-center cursor-pointer ${isActive ? "border-white text-white" : "border-neutral-800"}`}>
+                            <IconRenderer name={item.key} className='w-7 h-7' />
+                            <span className='ms-6 font-semibold'>{item.label}</span>
+                        </NavLink>
+                    ))}
+
+                    {/* <button className='w-72 h-12 pl-6 border-s-4 border-neutral-800 hover:border-white hover:text-white flex whitespace-nowrap items-center cursor-pointer'>
+                        <IconRenderer name="nonChargeable" className='w-7 h-7' />
+                        <span className='ms-6 font-semibold'>Non-Chargeable</span>
+                    </button>
+                    <button className='w-72 h-12 pl-6 border-s-4 border-neutral-800 hover:border-white hover:text-white flex whitespace-nowrap items-center cursor-pointer'>
+                        <IconRenderer name="chargeable" className='w-7 h-7' />
+                        <span className='ms-6 font-semibold'>Chargeable</span>
+                    </button>
+                    <button className='w-72 h-12 pl-6 border-s-4 border-neutral-800 hover:border-white hover:text-white flex whitespace-nowrap items-center cursor-pointer'>
+                        <IconRenderer name="settings" className='w-7 h-7' />
+                        <span className='ms-6 font-semibold'>Settings</span>
+                    </button> */}
+                </div>
             </div>
 
-            <div className='mb-20'>
-                <div className='w-72 h-12 pl-4 border-s-4 border-neutral-800 flex whitespace-nowrap items-center'>
+            <div>
+                <button onClick={toggleTheme} className='w-72 h-12 pl-6 my-3 border-s-4 border-neutral-800 hover:border-white hover:text-white flex whitespace-nowrap items-center cursor-pointer pr-12'>
+                    <IconRenderer name="moon" className='w-7 h-7' />
+                    <div className='flex items-center justify-between w-full'>
+                        <h1 className='ms-[26px] font-semibold'>Dark Mode</h1>
+                        <div className={`w-10 h-6 border-0 border-gray-400 rounded-full relative p-1 shadow-inner ${theme == 'dark' ? 'bg-blue-500' : 'bg-gray-400'}`}>
+                            <div className={`h-4 w-4 bg-white rounded-full transition-transform duration-500 shadow-lg ${theme == 'dark' ? 'translate-x-4' : 'translate-x-0'}`}></div>
+                        </div>
+                    </div>
+                </button>
+                <button onClick={handleLogout} className='w-72 h-12 pl-6 my-3 border-s-4 border-neutral-800 hover:border-white hover:text-white flex whitespace-nowrap items-center cursor-pointer'>
+                    <IconRenderer name="logout" className='w-7 h-7' />
+                    <span className='ms-6 font-semibold'>Log out</span>
+                </button>
+                <div className='pl-4 pr-8'>
+                    <hr className=''/>
+                </div>
+                <div className='w-72 h-12 pl-4 border-s-4 border-neutral-800 flex whitespace-nowrap items-center my-4'>
                     <img src="/default_profile_pic.png" alt="default_profile_pic" className='w-10 h-10 rounded-full' />
                     <div>
-                        <h1 className='ms-7 font-semibold leading-4'>John Arian Malondras</h1>
-                        <p className='ms-7 leading-4 text-xs'>Team Leader</p>
+                        <h1 className='ms-[21px] font-semibold leading-4 pr-8'>John Arian Malondras</h1>
+                        <p className='ms-[21px] leading-4 text-xs'>Team Leader</p>
                     </div>
                 </div>
             </div>
--- src/pages/non-chargeable/NonChargeableHome.jsx
+++ src/pages/non-chargeable/NonChargeableHome.jsx
@@ -1,17 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../../components/Button'
+import IconRenderer from '../../components/icons'
 
 const NonChargeableHome = () => {
+  const [search, setSearch] = useState('');
+
+  const handleSearch = () => {
+      let url = `?sort=${sort}&search=${search}`;
+      if(perPage != 25){
+          url += `&perPage=${perPage}`;
+      }
+      navigate(url, { replace: true });
+      getCollection();
+  };
+
+  const handleClearSearch = () => {
+      setSearch('');
+
+      let url = `?sort=${sort}`;
+      if(perPage != 25){
+          url += `&perPage=${perPage}`;
+      }
+      navigate(url, { replace: true });
+      getCollection();
+  };
+
   const handleTest = () => {
     console.log('test');
-    
   }
 
   return (
     <div className='bg-white h-full w-[calc(100%-96px)] rounded-r-2xl ml-24 pt-2 pr-2'>
-      <h1 className='text-xl font-bold'>Non Chargeable Requests</h1>
-      <div className=''>
-        <Button onClick={handleTest} className='w-40'>test</Button>
+      <h1 className='text-2xl font-bold dark:text-white'>Non Chargeable Requests</h1>
+      <div className='mt-3 flex justify-between items-center h-10'>
+        <Button onClick={handleTest} className='w-40 h-full'>ADD</Button>
+        <div className='flex h-full'>
+          <div className='h-full flex items-center gap-x-1 relative'>
+              <IconRenderer name={'search'} className='h-5 w-5 ml-2 absolute'></IconRenderer>
+              <input onChange={(e) => {setSearch(e.target.value)}} value={search} type="text" className='h-full rounded w-80 border border-gray-300 pl-8 dark:bg-gray-800 dark:border-gray-500' />
+              <div className='absolute right-2 flex items-center gap-x-1'>
+                  <button onClick={handleClearSearch} className='font-bold text-red-500'><IconRenderer name={'close'} className='h-5 w-5'></IconRenderer></button>
+                  <button onClick={handleSearch} className='font-medium border border-gray-300 dark:border-gray-500 rounded px-1 tracking-tight'>Search</button>
+              </div>
+          </div>
+          {/* <input type="text" className='border border-neutral-500 rounded h-full'/> */}
+        </div>
       </div>
     </div>
   )
