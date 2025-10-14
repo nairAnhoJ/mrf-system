a56a723e1f44fa51d6369c85c68f3aed8b891658
nairAnhoJ <malondrasarian@gmail.com>
2025-10-14 18:29:35 +0800

    Update Part Modal - Ongoing

--- src/components/UpdateParts.jsx
+++ src/components/UpdateParts.jsx
@@ -30,6 +30,7 @@ const UpdateParts = ({closeButton, approveSuccess, id, parts}) => {
                         name: part.name,
                         brand_id: part.brand_id,
                         brand: part.brand,
+                        price: part.price,
                     }
                 ]
             )
@@ -82,7 +83,7 @@ const UpdateParts = ({closeButton, approveSuccess, id, parts}) => {
     return (
         <>
             <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-gray-900/30 z-100">
-                <aside className='bg-white dark:bg-neutral-600 w-[90vw] min-w-[500px] rounded text-neutral-600 dark:text-neutral-50'>
+                <aside className='bg-white dark:bg-neutral-600 w-[95vw] min-w-[500px] rounded text-neutral-600 dark:text-neutral-50'>
                     <div className='w-full p-6 border-b border-neutral-300 text-xl leading-5 font-bold flex justify-start items-center'>
                         <h1>Update Parts</h1>
                     </div>
@@ -92,25 +93,29 @@ const UpdateParts = ({closeButton, approveSuccess, id, parts}) => {
                             <h1 className='text-center w-56 font-bold whitespace-nowrap'>Part Number</h1>
                             <h1 className='flex-1 text-center font-bold'>Description</h1>
                             <h1 className='text-center w-52 font-bold'>Brand</h1>
+                            <h1 className='text-center w-40 font-bold whitespace-nowrap'>Price (₱)</h1>
                         </div>
-                        {
-                            updatedParts.map((part, index) => (
-                                <div key={index} className='mt-3'>
-                                    <div className='flex gap-x-3'>
-                                        <input type="text" name='item_number' className={`border border-neutral-500 rounded p-1 text-center text-sm w-40 ${errors.find((err) => err.prop == 'item_number' && err.index == index) ? 'border-red-500' : ''} `} onChange={(e) => handleOnChange(part.id, e.target.name, e.target.value)} value={part.item_number}/>
-                                        <input type="text" name='number' className={`border border-neutral-500 rounded p-1 text-center text-sm w-56 ${errors.find((err) => err.prop == 'number' && err.index == index) ? 'border-red-500' : ''} `} onChange={(e) => handleOnChange(part.id, e.target.name, e.target.value)} value={part.number}/>
-                                        <input type="text" name='name' className={`border border-neutral-500 rounded p-1 text-sm flex-1 ${errors.find((err) => err.prop == 'name' && err.index == index) ? 'border-red-500' : ''} `} onChange={(e) => handleOnChange(part.id, e.target.name, e.target.value)} value={part.name}/>
-                                        <select name="brand_id" className='border border-neutral-500 rounded p-1 text-center text-sm w-52' onChange={(e) => handleOnChange(part.id, e.target.name, e.target.value)} value={part.brand_id}>
-                                            {
-                                                partBrands.map((brand, index) => (
-                                                    <option key={index} value={brand.id}>{brand.name}</option>
-                                                ))
-                                            }
-                                        </select>
+                        <div className='max-h-96 overflow-y-auto overflow-x-hidden'>
+                            {
+                                updatedParts.map((part, index) => (
+                                    <div key={index} className='mt-3'>
+                                        <div className='flex gap-x-3'>
+                                            <input type="text" name='item_number' className={`border border-neutral-500 rounded p-1 text-center text-sm w-40 ${errors.find((err) => err.prop == 'item_number' && err.index == index) ? 'border-red-500' : ''} `} onChange={(e) => handleOnChange(part.id, e.target.name, e.target.value)} value={part.item_number}/>
+                                            <input type="text" name='number' className={`border border-neutral-500 rounded p-1 text-center text-sm w-56 ${errors.find((err) => err.prop == 'number' && err.index == index) ? 'border-red-500' : ''} `} onChange={(e) => handleOnChange(part.id, e.target.name, e.target.value)} value={part.number}/>
+                                            <input type="text" name='name' className={`border border-neutral-500 rounded p-1 text-sm flex-1 ${errors.find((err) => err.prop == 'name' && err.index == index) ? 'border-red-500' : ''} `} onChange={(e) => handleOnChange(part.id, e.target.name, e.target.value)} value={part.name}/>
+                                            <select name="brand_id" className='border border-neutral-500 rounded p-1 text-center text-sm w-52' onChange={(e) => handleOnChange(part.id, e.target.name, e.target.value)} value={part.brand_id}>
+                                                {
+                                                    partBrands.map((brand, index) => (
+                                                        <option key={index} value={brand.id}>{brand.name}</option>
+                                                    ))
+                                                }
+                                            </select>
+                                            <input type="number" name='price' className={`border border-neutral-500 rounded p-1 text-center text-sm w-40 ${errors.find((err) => err.prop == 'price' && err.index == index) ? 'border-red-500' : ''} `} onChange={(e) => handleOnChange(part.id, e.target.name, e.target.value)} value={part.price}/>
+                                        </div>
                                     </div>
-                                </div>
-                            ))
-                        }
+                                ))
+                            }
+                        </div>
                     </div>
                     <div className='w-full p-6 border-t border-neutral-300 flex gap-x-3'>
                         <Button color='blue' onClick={handleYes} className={"w-40"}>Update</Button>
